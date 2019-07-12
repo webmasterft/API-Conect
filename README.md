b971ca1db667b51042548a2813114b1c3cceb26a
Fernando Torres <webmasterft@users.noreply.github.com>
2019-07-12 17:18:37 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# API-Conect
-Created with CodeSandbox
---
+++ src/Components/app.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Card } from "./cards";
+import { Search } from "./searchbox";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onChildClicked = this.onChildClicked.bind(this);
+    this.state = {
+      cod: "",
+      city: "Quito",
+      items: [],
+      isLoaded: false
+    };
+  }
+  onChildClicked(value) {
+    //this.setState({ city: value });
+    this.getWeather(value);
+  }
+
+  getWeather = async value => {
+    return await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/forecast/?q=${value}&&units=metric&appid=55f878d73891e78f48f3d80c558ea579`
+      )
+      .then(data => {
+        var dataChuncked = [],
+          dataFetched = data.data;
+        for (let i = 1; i < dataFetched.list.length; i += 8) {
+          dataChuncked.push(dataFetched.list[i]);
+        }
+        this.setState({
+          isLoaded: true,
+          city: dataFetched.city.name,
+          items: dataChuncked
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    //this.getWeather(this.state.city);
+    console.log(prevProps.city, prevState.city);
+    if (prevProps.city !== prevState.city) {
+      console.log(this.state);
+      //this.setState({ city: prevProps.city });
+      //this.getWeather(this.state.city);
+    }
+  }
+  componentDidMount() {
+    this.getWeather(this.state.city);
+  }
+
+  render() {
+    var { isLoaded, items, city } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div className="App">
+        <div className="container">
+          <Search func={this.onChildClicked} />
+          <p className="cityName">{city}</p>
+          {items.map(item => (
+            <Card key={item.dt} item={item} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
